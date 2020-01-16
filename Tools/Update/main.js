a36df2ce33c52fa94491fdd565c532b15af05773
nb5p <2098464+nb5p@users.noreply.github.com>
2020-01-16 09:52:34 +0800

    add telegram list update script

---
+++ Tools/Update/main.js
@@ -0,0 +1,31 @@
+const nunjucks = require('nunjucks');
+const axios = require('axios');
+const agent = require('global-agent');
+const fs = require("fs");
+
+agent.bootstrap();
+axios.get('https://core.telegram.org/resources/cidr.txt')
+    .then(function (response) {
+        resData = response.data;
+        arrayIP = resData.split('\n').map(x => x.trim()).filter(x => x);
+        IPv4Set = [];
+        IPv6Set = [];
+        for (itermIP of arrayIP) {
+            if (itermIP.trim()) {
+                if (itermIP.indexOf(".") != -1) {
+                    IPv4Set.push(itermIP);
+                } else {
+                    IPv6Set.push(itermIP);
+                }
+            }
+        }
+        nunjucks.configure({ autoescape: true });
+        telegramText = nunjucks.render(`${__dirname}/Template/telegram.njk`, { IPv4Set, IPv6Set });
+        fs.writeFile(`${__dirname}/../../Surge/Rule-Set/Telegram.list`, telegramText, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+        });
+    }).catch(function (err) {
+        console.error(err);
+    });
