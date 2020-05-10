53c3997bdb413363b65060eaa1ca2e27fbe1a8f1
nb5p <2098464+nb5p@users.noreply.github.com>
2020-05-10 22:03:53 +0800

    Update Rules

--- Tools/Update/main.js
+++ Tools/Update/main.js
@@ -7,29 +7,34 @@ agent.bootstrap();
 
 // Telegram.list
 axios.get('https://core.telegram.org/resources/cidr.txt')
-    .then(function (response) {
-        resData = response.data;
-        arrayIP = resData.split('\n').map(x => x.trim()).filter(x => x);
-        IPv4Set = [];
-        IPv6Set = [];
-        for (itermIP of arrayIP) {
-            if (itermIP.trim()) {
-                if (itermIP.indexOf(".") != -1) {
-                    IPv4Set.push(itermIP);
-                } else {
-                    IPv6Set.push(itermIP);
-                }
-            }
+  .then(function (response) {
+    resData = response.data;
+    arrayIP = resData.split('\n').map(x => x.trim()).filter(x => x);
+    IPv4Set = [];
+    IPv6Set = [];
+    for (itermIP of arrayIP) {
+      if (itermIP.trim()) {
+        if (itermIP.indexOf(".") != -1) {
+          IPv4Set.push(itermIP);
+        } else {
+          IPv6Set.push(itermIP);
         }
-        nunjucks.configure({ autoescape: true });
-        telegramText = nunjucks.render(`${__dirname}/Template/telegram.njk`, { IPv4Set, IPv6Set });
-        fs.writeFile(`${__dirname}/../../Surge/RuleSet/Telegram.list`, telegramText, function (err) {
-            if (err) {
-                return console.error(err);
-            }
-        });
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1)
+      }
+    }
+    nunjucks.configure({
+      autoescape: true
     });
+    telegramText = nunjucks.render(`${__dirname}/Template/telegram.njk`, {
+      IPv4Set,
+      IPv6Set
+    });
+    fs.writeFile(`${__dirname}/../../Surge/RuleSet/Telegram.list`, telegramText, function (err) {
+      if (err) {
+        return console.error(err);
+      }
+    });
+  })
+  .catch(function (err) {
+    console.error(err);
+    process.exit(1)
+  });
